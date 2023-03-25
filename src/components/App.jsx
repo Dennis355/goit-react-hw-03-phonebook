f877cb2ae2bc24ce8415b89eb4ead424653df384
Dennis355 <Snidevichden@gmail.com>
2023-03-25 09:44:41 +0200

    fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,90 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { ContactList } from 'components/ContactList/ContactList';
+import PhoneForm from 'components/PhoneForm/PhoneForm';
+import { Filter } from 'components/Filter/filter';
+import css from 'components/PhonebookStart.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Aline Copand', number: '227-96-26' },
+      { id: 'id-6', name: 'Appolina Copander', number: '227-96-25' },
+      { id: 'id-7', name: 'Nigera Coopera', number: '225-96-25' },
+      { id: 'id-8', name: 'Regina Cobra', number: '25-96-25' },
+      { id: 'id-9', name: 'Edena Kliente-Cobra', number: '125-96-25' },
+    ],
+    filter: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+
+    const contactsLocal = localStorage.getItem('contacts');
+    const normalcontacts = JSON.parse(contactsLocal);
+
+    this.setState({ contacts: normalcontacts });
+  }
+
+  addContact = ({ name, number }) => {
+    const contact = { id: nanoid(), name: name, number: number };
+
+    const names = this.state.contacts.map(contact =>
+      contact.name.toLowerCase()
+    );
+
+    if (names.includes(name.toLowerCase())) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  getVisiblefilter = () => {
+    const { filter, contacts } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visiblefilter = this.getVisiblefilter();
+
+    return (
+      <div className={css.phonebook_contacts_block}>
+        <PhoneForm onSubmit={this.addContact} />
+        <h2 className={css.phonebook_title_h2}> Contacts</h2>{' '}
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visiblefilter}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/PhoneForm/PhoneForm.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+import css from 'components/PhoneForm/PhoneForm.module.css';
+
+class PhoneForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handlerInputChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  addContactForm = e => {
+    e.preventDefault();
+    console.log(this.state);
+
+    this.props.onSubmit(this.state);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <div className={css.div_phonebook_full}>
+        <section className={css.section_phonenumber}>
+          <form onSubmit={this.addContactForm}>
+            <section className={css.section_search_add}>
+              <div className={css.phonebook_title}>
+                <h2 className={css.phonebook_title_h2}> Phonebook </h2>{' '}
+              </div>
+              <div className={css.phonebook_block_search}>
+                <label className={css.phonebook_block_title}>
+                  {' '}
+                  Name
+                  <input
+                    className={css.input_search}
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                    value={this.state.name}
+                    onChange={this.handlerInputChange}
+                  />
+                </label>
+              </div>
+            </section>
+
+            <section className={css.section_phonenumber}>
+              <label className={css.phonebook_block_title}>
+                {' '}
+                Number
+                <input
+                  className={css.input_search}
+                  type="tel"
+                  name="number"
+                  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                  required
+                  onChange={this.handlerInputChange}
+                  value={this.state.number}
+                />
+              </label>
+            </section>
+
+            <button type="submit" className={css.phonebook_btn_addcontact}>
+              Add contacts{' '}
+            </button>
+          </form>
+        </section>
+      </div>
+    );
+  }
+}
+export default PhoneForm;
